7ad71f391ee70542ee107bd8637e082f14e0597d
Florian Stevens <fstevens@schueler.hakzell.at>
2021-04-28 08:36:10 +0200

    abgabe

---
+++ Vorlage.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vorlage.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        //Frage1: Durch update source trigger
+
+    }
+}
